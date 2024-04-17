9a1e3d232a9cf5cb5a8c02efb5e46e50cc86de49
Aaron Beckles <therealini@gmail.com>
2024-04-17 17:15:41 -0400

    added markdown generator

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// This function takes the inquirer answers and returns a string that can be written to a readme file
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${data.license.badge}
 
+# ${data.title}
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Description
+${data.description}
+
+## Installation
+${data.installationInstructions}
+
+## Usage
+${data.usageInformation}
+
+## License
+This project is covered under the ${data.license.label} license.
+
+## Contributing
+${data.contributionGuidelines}
+
+## Tests
+${data.testInstructions}
+
+## Questions
+[Github profile](https://github.com/${data.githubUsername})
+Email me at ${data.email} if you have additional questions.
 `;
 }
 
